062b8186ca593f2405ec8e8440793a2d633fdb45
EvvJaz <evolet.arellano.avila@cbtis89.edu.mx>
2024-10-21 09:30:02 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,18 @@
+<?php
+/* CBTIS 89
+   P00Presentación
+   Programacontiene mis datos personales
+   Evolet Jazmìn Arellano Àvila
+   3ªA Programaciòn Matutino
+*/
+
+echo "Nombre: Evolet Jazmín <br>";
+echo "Apellidos: Arellano Ávila <br>";
+echo "Fecha de nacimiento: 15/05/2008 <br>";
+echo "Edad: 16 años <br>";
+echo "Ubicacion: Durango, Dgo. <br>";
+echo "Escuela: CBTIS89 <br>";
+echo "Carrera: Programación <br>";
+echo "Grado y grupo: 3ºA <br>";
+
+?>
