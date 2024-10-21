145dfe9e97bacedbe82faa260fcc7309977db3f9
EvvJaz <evolet.arellano.avila@cbtis89.edu.mx>
2024-10-21 09:10:09 -0600

    Add files via upload

---
+++ P01Variables1.php
@@ -0,0 +1,25 @@
+<?php
+/* CBTIS 89
+   Programa que imprime el dìa, sueldo y nombre
+   Evolet Jazmìn Arellano Àvila
+   3ªA Programaciòn Matutino
+*/
+
+$dia = 24; //Se declara una variable de tipo integer.
+$sueldo = 143.43; //Se declara una variable de tipo double.
+$nombre = "Linos"; //Se declara una varieble de tipo string.
+$exite = true; //Se declara una varieble boolean.
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Varieble string:";
+echo $nombre;echo "<br>";
+echo "Varieble boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "mundo";
+echo $cadena1. "       ".$cadena2;
+?>
